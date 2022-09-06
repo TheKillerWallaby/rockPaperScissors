eff87b09cd99c25ca61f4b175c91cd28f3738dd7
TheKillerWallaby <dufresnec@gmail.com>
2022-09-06 19:52:42 -0400

    Added a Switch statement to getComputerChoice function

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+let computerChoice;
 function getComputerChoice() {
-    let computerChoiceNum = Math.floor(Math.random() * 3);
+    //let computerChoiceNum = Math.floor(Math.random() * 3);
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors";
+            break;
+    }
     
-    
-    console.log(computerChoiceNum);
+    console.log(computerChoice);
 } 
 
 getComputerChoice();
