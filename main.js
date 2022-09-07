14d5880486870ca8a97df1231f42716ba85bffe1
TheKillerWallaby <dufresnec@gmail.com>
2022-09-07 18:14:05 -0400

    Added five 5 rounds and keeps score

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let computerChoice;
 let playerChoice; 
+let computerWin = 0;
+let playerWin = 0;
+let ties = 0;
+let errors = 0;
 function getComputerChoice() {
     switch (Math.floor(Math.random() * 3)) {
         case 0:
@@ -25,20 +29,46 @@ function getPlayerSelection() {
 function playRound() {
     if (computerChoice == playerChoice) {
         console.log("Its a tie!");
+        ties++;
     } else if (computerChoice == "ROCK" && playerChoice == "SCISSORS") {
         console.log("You lose! " + computerChoice + " beats " + playerChoice);
+        computerWin++;
     } else if (computerChoice == "SCISSORS" && playerChoice == "PAPER") {
-        cconsole.log("You lose! " + computerChoice + " beats " + playerChoice);
+        console.log("You lose! " + computerChoice + " beats " + playerChoice);
+        computerWin++;
     } else if (computerChoice == "PAPER" && playerChoice == "ROCK") {
-        cconsole.log("You lose! " + computerChoice + " beats " + playerChoice);
+        console.log("You lose! " + computerChoice + " beats " + playerChoice);
+        computerWin++;
     } else if (computerChoice == "SCISSORS" && playerChoice == "ROCK") {
         console.log("You win! " + playerChoice + " beats " + computerChoice);
+        playerWin++;
     } else if (computerChoice == "PAPER" && playerChoice == "SCISSORS") {
         console.log("You win! " + playerChoice + " beats " + computerChoice);
+        playerWin++;
     } else if (computerChoice == "ROCK" && playerChoice == "PAPER") {
         console.log("You win! " + playerChoice + " beats " + computerChoice);
-    } else {"Something went wrong! " + computerChoice + " " + playerChoice}
+        playerWin++;
+    } else {console.log("Something went wrong! " + computerChoice + " " + playerChoice)
+        errors++;
+    }
 }
-getComputerChoice();
-getPlayerSelection();
-playRound(playerChoice, computerChoice);
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        getComputerChoice();
+        getPlayerSelection();
+        playRound(playerChoice, computerChoice);
+        
+        console.log("Player Wins: " + playerWin + "\n" + "Computer Wins: " + computerWin + "\n" + "Ties: " + ties);
+    }
+
+    if (playerWin > computerWin) {
+        console.log("Player Wins the Game!");
+    } else if (computerWin > playerWin) {
+        console.log("Computer Wins the Game!");
+    } else { console.log("It's a tie!");
+    }
+
+}
+
+game();
